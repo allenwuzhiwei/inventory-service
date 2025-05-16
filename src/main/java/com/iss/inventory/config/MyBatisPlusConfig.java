452ff1c99e3c0d4b57fec656b96a3674736f7702
allenwuzhiwei <allenzhiwei0@gmail.com>
2025-05-16 22:27:48 +0800

    first commit

---
+++ src/main/java/com/iss/inventory/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.iss.inventory.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.nusiss.productservice.dao") // 指定扫描 Mapper 接口的包
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // use correct database type
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
