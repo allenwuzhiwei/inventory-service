fb535c20c4e01c5fa0aaed1666e533e134d9ad3f
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-05-23 17:40:36 +0800

    Fix incorrect @MapperScan path in MyBatisPlusConfig to properly load inventory mappers

--- src/main/java/com/iss/inventory/config/MyBatisPlusConfig.java
+++ src/main/java/com/iss/inventory/config/MyBatisPlusConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.nusiss.productservice.dao") // 指定扫描 Mapper 接口的包
+@MapperScan("com.iss.inventory.mapper") // 指定扫描 Mapper 接口的包
 public class MyBatisPlusConfig {
 
     @Bean
